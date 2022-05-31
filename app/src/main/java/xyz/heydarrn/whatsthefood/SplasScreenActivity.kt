75004c134f61cbb95ba65be79437ed4f16748927
Noorman <noorman.np.ac@gmail.com>
2022-05-31 14:37:30 +0700

    edit SplasScreenActivity.kt

--- app/src/main/java/xyz/heydarrn/whatsthefood/SplasScreenActivity.kt
+++ app/src/main/java/xyz/heydarrn/whatsthefood/SplasScreenActivity.kt
@@ -1,11 +1,20 @@
 package xyz.heydarrn.whatsthefood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplasScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splas_screen)
+
+        val TIMES_MILES = 1000L
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, TIMES_MILES)
     }
 }
