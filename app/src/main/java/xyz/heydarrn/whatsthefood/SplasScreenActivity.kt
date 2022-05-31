02efdc97752c827c3459ce6ca8ccdebf8f9081c6
Noorman <noorman.np.ac@gmail.com>
2022-05-31 14:32:41 +0700

    add SplasScreenActivity.kt

---
+++ app/src/main/java/xyz/heydarrn/whatsthefood/SplasScreenActivity.kt
@@ -0,0 +1,11 @@
+package xyz.heydarrn.whatsthefood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplasScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splas_screen)
+    }
+}
